3b8667d1d4d92d40ede16628b6f27d6423916eab
Xinxi Zhang <xz657@scarletmail.rutgers.edu>
2023-03-30 11:45:56 -0400

    delete test.py

--- segmentation/data_utils/test.py
+++ segmentation/data_utils/test.py
@@ -1,37 +1,54 @@
 import numpy as np
 import torch
-def find_nearest_neighbors(pc_t, pc_t_1, pc_t_2):
-    # Convert numpy arrays to torch tensors
-    pc_t_xyz = torch.tensor(pc_t[:, :3]).float()  # Only use x, y, z coordinates for distance calculations
-    pc_t_1_xyz = torch.tensor(pc_t_1[:, :3]).float()
-    pc_t_2_xyz = torch.tensor(pc_t_2[:, :3]).float()
+from knn_cuda import KNN
 
-    # Calculate the distance matrix
-    distance_matrix_1 = torch.cdist(pc_t_xyz, pc_t_1_xyz)
-    distance_matrix_2 = torch.cdist(pc_t_xyz, pc_t_2_xyz)
+# The provided KNN library code should be here
 
-    # Find the indices of the nearest neighbors in pc_t-1 and pc_t-2
-    nearest_neighbor_idx_1 = torch.argmin(distance_matrix_1, dim=1)
-    nearest_neighbor_idx_2 = torch.argmin(distance_matrix_2, dim=1)
+def find_nearest_neighbors_batched(sequences, k=1):
+    batch_size, n, num_points, _ = sequences.shape
 
-    # Get nearest neighbors from pc_t-1 and pc_t-2 using the indices
-    nearest_neighbors_1 = pc_t_1[nearest_neighbor_idx_1.numpy()]
-    nearest_neighbors_2 = pc_t_2[nearest_neighbor_idx_2.numpy()]
+    # Move sequences to GPU
+    sequences = torch.tensor(sequences).float().cuda()
 
-    # Concatenate pc_t with nearest neighbors from pc_t-1 and pc_t-2
-    result = np.hstack((pc_t, nearest_neighbors_1, nearest_neighbors_2))
+    # Instantiate KNN module
+    knn_module = KNN(k=k, transpose_mode=True)
+
+    # Initialize the result tensor
+    result = torch.zeros(batch_size, num_points, 4 * n).cuda()
+
+    for b in range(batch_size):
+        for i in range(1, n):
+            pc_t = sequences[b, i]
+            pc_prev = sequences[b, i - 1]
+
+            # Find nearest neighbors in pc_prev
+            _, nearest_neighbor_idx = knn_module(pc_prev[:, :3].unsqueeze(0), pc_t[:, :3].unsqueeze(0))
+
+            # Move the nearest neighbor indices back to the CPU
+            nearest_neighbor_idx = nearest_neighbor_idx.cpu().numpy().squeeze()
+
+            # Get nearest neighbors from pc_prev using the indices
+            nearest_neighbors = pc_prev.cpu().numpy()[nearest_neighbor_idx]
+
+            # Concatenate pc_t with nearest neighbors
+            result[b, :, 4 * i:4 * (i + 1)] = torch.tensor(nearest_neighbors).cuda()
+
+        # Set pc_t as the first 4 columns of the result
+        result[b, :, :4] = sequences[b, 0]
+
+    # Move the result back to the CPU
+    result = result.cpu().numpy()
 
     return result
 
-"""# Test the function
+
+# Test the function
+B = 2
+n = 3
 num_points = 100
 
-# Generate random point clouds
-pc_t = np.random.rand(num_points, 4)
-pc_t_1 = np.random.rand(num_points, 4)
-pc_t_2 = np.random.rand(num_points, 4)
+# Generate random point cloud sequences
+sequences = np.random.rand(B, n, num_points, 4)
 
-result = find_nearest_neighbors(pc_t, pc_t_1, pc_t_2)
-print(result.shape)  # Should output (num_points, 4 * 3)"""
-
-print(torch.cuda.is_available())
+result = find_nearest_neighbors_batched(sequences)
+print(result.shape)  # Should output (B, num_points, 4 * n)
